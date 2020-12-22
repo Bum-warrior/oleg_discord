0db77b1ce1506539887f730e9e2c1bd4feae2a9a
Bum-warrior <69102918+Bum-warrior@users.noreply.github.com>
2020-12-23 00:11:02 +0300

    2 files not uploaded
    I add this

---
+++ config.py
@@ -0,0 +1,29 @@
+token = 'TOKEN'
+
+admin = ['F_Lone#2220']
+
+cat_pics = [
+    'https://w.wallhaven.cc/full/13/wallhaven-133yd3.jpg',
+    'https://w.wallhaven.cc/full/2e/wallhaven-2e3wwg.jpg',
+    'https://w.wallhaven.cc/full/5d/wallhaven-5d9kp9.jpg'
+]
+luck = ['F_Lone#2220']
+
+ban_word = ["loh", "lox", "лох", "лах"]
+
+gachi = ["gachi", "right version", "гачи", "finger"]
+
+iam = [
+    'https://media.giphy.com/media/Xzmr06eX5nGUg/giphy.gif',
+    'https://media.giphy.com/media/hNj7x7JMQI1qg/giphy.gif',
+    'https://media.giphy.com/media/rnHxGiRAvsPTi/giphy.gif'
+    ]
+
+
+boobs = [
+    'https://media.giphy.com/media/10qyRyUZDvf6RW/giphy.gif',
+    'https://media.giphy.com/media/QscGFjzLHXVg4/giphy.gif',
+    'https://media.giphy.com/media/KmBd3r88LGrqE/giphy.gif'
+]
+
+diablo_hero = ['Варвар', 'Крестоносец', 'Охотник на демонов', 'Монах', 'Колдун', 'Чародейка']
